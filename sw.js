b6970fbbeec578914828e2f7adadc6e01541c77d
nine-glitch <laura.amarillaa@gmail.com>
2026-02-28 10:29:13 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
-const CACHE = 'carlitos-v1';
+const CACHE = 'carlitos-v2';
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(['/']))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
